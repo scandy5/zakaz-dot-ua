28e6d0389919262f911fa5f39d6f91adf329556d
Sergey <scandy_boss@ukr.net>
2019-02-26 20:52:31 +0200

    Updates

---
+++ precache-manifest.93755fe567e1ad8fd28a967d2edf79c7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5a772f5f6632c2745f63718ca0ff2c40",
+    "url": "/zakaz-dot-ua/static/media/logo.5a772f5f.svg"
+  },
+  {
+    "revision": "f1b0a00c35c9f7e5b12cae5f7fe6af33",
+    "url": "/zakaz-dot-ua/static/media/cart.f1b0a00c.svg"
+  },
+  {
+    "revision": "b04451fc5d6deb1d8710",
+    "url": "/zakaz-dot-ua/static/js/runtime~main.b04451fc.js"
+  },
+  {
+    "revision": "ee06c7958cf592432571",
+    "url": "/zakaz-dot-ua/static/js/main.ee06c795.chunk.js"
+  },
+  {
+    "revision": "35770fccc80c86e5937e",
+    "url": "/zakaz-dot-ua/static/js/2.35770fcc.chunk.js"
+  },
+  {
+    "revision": "ee06c7958cf592432571",
+    "url": "/zakaz-dot-ua/static/css/main.0bce5999.chunk.css"
+  },
+  {
+    "revision": "67844c7f57f08ab519a9c8e133b03d9c",
+    "url": "/zakaz-dot-ua/index.html"
+  }
+];
